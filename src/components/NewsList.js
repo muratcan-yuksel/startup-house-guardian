df64241b6bfd6e40dc85ae5e90be8c0c4928a863
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-19 02:33:16 +0300

    fallback

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -7,10 +7,10 @@ const NewsList = () => {
       <div className="border border-gray-400 border-opacity-40 mx-4 md:mx-0 w-80 md:w-96 p-5 rounded-md shadow-md">
         <h3 className="text-2xl text-[#606c76]">Sample news title</h3>
         <ul className="list-disc list-inside text-gray-500 p-5">
-          <li className="text-sm font-bold">
+          <li className="text-sm font-bold pb-3">
             Section Name: <span className="font-normal">Sample Section</span>{" "}
           </li>
-          <li className="text-sm font-bold">
+          <li className="text-sm font-bold pb-3">
             Publication Date: <span className="font-normal">06.12.2018</span>
           </li>
         </ul>
--- src/components/ReadLater.js
+++ src/components/ReadLater.js
@@ -3,15 +3,15 @@ import React from "react";
 const ReadLater = () => {
   return (
     <div className="">
-      <ul className="list-disc list-inside text-gray-500 p-5">
-        <li className="text-2xl text-[#606c76]">Sample news title</li>
+      <ul className="list-disc list-inside text-gray-500 px-5 py-1">
+        <li className="text-xl font-light text-[#606c76]">Sample news title</li>
       </ul>
       <div className="flex flex-row justify-start">
-        <button className=" text-[#9b4dca] hover:text-[#606c76] ml-5 px-7 py-2">
-          <span className="text-xs font-bold">FULL ARTICLE</span>
+        <button className=" text-[#9b4dca] hover:text-[#606c76] ml-5 px-7 ">
+          <span className="text-xs font-bold">Read</span>
         </button>
-        <button className="text-[#9b4dca] hover:text-[#606c76]  px-7 py-2">
-          <span className="text-xs font-bold">READ LATER</span>
+        <button className="text-[#9b4dca] hover:text-[#606c76]  px-7 ">
+          <span className="text-xs font-bold">Remove</span>
         </button>
       </div>
     </div>
