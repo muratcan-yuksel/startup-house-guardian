a835adf995665df1763cc6c9eeec1f71e4a6ec1a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-18 17:24:56 +0300

    style arrows

--- src/App.js
+++ src/App.js
@@ -1,67 +1,73 @@
 import React from "react";
 import Header from "./components/Header";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Header />
       {/* wrapper */}
-      <div className="flex flex-col">
-        {/* item */}
-        <div className="flex flex-col md:flex-row mx-4">
-          <div className="flex flex-col md:w-6/12 w-full mb-4 mr-0 md:mr-2">
-            <label
-              htmlFor="newsContentSearch"
-              className="font-bold text-[#606c76] mb-2"
-            >
-              News content search
-            </label>
-            <input
-              type="search"
-              placeholder="News content search"
-              id="newsContentSearch"
-              className="border rounded-md h-9 p-4 font-light"
-            />
-          </div>
+      <div className="flex justify-center">
+        <div className="flex flex-col w-full  lg:w-7/12">
           {/* item */}
-          <div className="flex flex-col md:w-6/12 w-full mb-4 ml-0 md:ml-2">
+          <div className="flex flex-col md:flex-row mx-4">
+            <div className="flex flex-col md:w-6/12 w-full mb-4 mr-0 md:mr-2">
+              <label
+                htmlFor="newsContentSearch"
+                className="font-bold text-[#606c76] mb-2"
+              >
+                News content search
+              </label>
+              <input
+                type="search"
+                placeholder="News content search"
+                id="newsContentSearch"
+                className="border rounded-md h-9 p-4 font-light"
+              />
+            </div>
+            {/* item */}
+            <div className="flex flex-col md:w-6/12 w-full mb-4 ml-0 md:ml-2">
+              {" "}
+              <label
+                htmlFor="sectionSelect"
+                className="font-bold text-[#606c76] mb-2"
+              >
+                Section
+              </label>
+              <select
+                id="sectionSelect"
+                className="bg-[#ffffff] border rounded-md h-9 font-light px-4"
+              >
+                <option value="all">All</option>
+                <option value="books">Books</option>
+                <option value="business">Business</option>
+                <option value="culture">Culture</option>
+                <option value="Sport">Sport</option>
+              </select>
+            </div>
+          </div>
+          {/* top section ends */}
+          <div className="flex flex-col w-1/6 mx-4">
             {" "}
             <label
-              htmlFor="sectionSelect"
+              htmlFor="activePageSelect"
               className="font-bold text-[#606c76] mb-2"
             >
-              Section
+              Active Page
             </label>
             <select
-              id="sectionSelect"
-              className="bg-[#ffffff] border rounded-md h-9 font-light"
+              id="activePageSelect"
+              className="bg-[#ffffff] border rounded-md h-9 font-light px-4"
+              onChange={(e) => {
+                console.log(e.target.value);
+              }}
             >
-              <option value="all">All</option>
-              <option value="books">Books</option>
-              <option value="business">Business</option>
-              <option value="culture">Culture</option>
-              <option value="Sport">Sport</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
             </select>
           </div>
-        </div>
-        {/* top section ends */}
-        <div className="flex flex-col w-1/6 mx-4">
-          {" "}
-          <label
-            htmlFor="activePageSelect"
-            className="font-bold text-[#606c76] mb-2"
-          >
-            Active Page
-          </label>
-          <select
-            id="activePageSelect"
-            className="bg-[#ffffff] border rounded-md h-9 font-light"
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-          </select>
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import logo from "../assets/startuphouselogo.png";
 
 const Header = () => {
   return (
-    <div className="bg-[#f4f5f6] shadow-[3px_0_6px_0_#c9ced5] p-5">
-      <img className="h-10 mr-7" src={logo} alt="" />
+    <div className="bg-[#f4f5f6] shadow-[3px_0_6px_0_#c9ced5] p-5 flex justify-start lg:justify-center">
+      <div className="w-7/12 ">
+        <img className="h-10 mr-7" src={logo} alt="" />
+      </div>
     </div>
   );
 };
